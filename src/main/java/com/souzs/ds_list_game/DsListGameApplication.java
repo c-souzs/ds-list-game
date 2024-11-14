e5be2670cf19d87bec5c6d0476fd3890936ecff7
Caio Souza <souzsdev@gmail.com>
2024-11-14 19:22:09 -0300

    Initial commit

---
+++ src/main/java/com/souzs/ds_list_game/DsListGameApplication.java
@@ -0,0 +1,13 @@
+package com.souzs.ds_list_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListGameApplication.class, args);
+	}
+
+}
