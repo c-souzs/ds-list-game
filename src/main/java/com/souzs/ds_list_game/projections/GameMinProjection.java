61f23846fa9fe24c42e859d776125c4e8ab5222d
Caio Souza <souzsdev@gmail.com>
2024-11-16 11:34:06 -0300

    aula-03
    Configuracoes para ambiente de desenvolvimento

--- src/main/java/com/souzs/ds_list_game/projections/GameMinProjection.java
+++ src/main/java/com/souzs/ds_list_game/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.souzs.ds_list_game.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
